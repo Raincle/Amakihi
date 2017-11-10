3dc076673150e3b95de1bf878f8378cfe8191434
Hong <hongyawei@yit.com>
2017-11-10 16:55:51 +0800

    0.2

--- domains/baidu.js
+++ domains/baidu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amakihi for Baidu
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Give You a Purification of Baidu!
 // @author       Hong
 // @match        https://www.baidu.com/*
@@ -11,7 +11,7 @@
 (function() {
     'use strict';
     
-    $("#u, #u1, #lg, .qrcodeCon, #ftCon").remove();
+    $("#u, #u1, #lg, .qrcodeCon, #ftCon, #content_right").remove();
     $("#result_logo").css("opacity", 0);
     document.title = "搜索";
     $("#su").val("搜索");
